f62dd6cd87a9ae0ac76a34430cc9e2c3e0b91f20
TJ Mason <tmason312@protonmail.com>
2025-12-23 09:47:42 -0500

    add list, tuple, and tabulate for data handling/display

--- mmh.py
+++ mmh.py
@@ -1,45 +1,49 @@
 import sys
+import pprint
+from typing import List, Tuple
+from tabulate import tabulate
 
 NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 MODES = ['Ionian', 'Dorian', 'Phrygian', 'Lydian', 'Mixolydian', 'Aeolian', 'Locrian']
 CHORDS = ['maj', 'min', 'min', 'maj', 'maj', 'min', 'dim']
-IONIAN_INTERVALS = [2, 2, 1, 2, 2, 2, 1]
+IONIAN_STEPS = [2, 2, 1, 2, 2, 2, 1]
 
-def get_note_offset(key):
+def get_note_offset(key: str) -> int:
     if key in NOTES:
         return NOTES.index(key)
     else:
         print(f"invalid key: {key}")
         sys.exit(1)
 
-def get_modal_offset(mode):
+def get_modal_offset(mode: str) -> int:
     if mode in MODES:
         return MODES.index(mode)
     else:
         print(f"invalid mode supplied: {mode}")
+        sys.exit(1)
 
-def print_key(key):
-    print(f"Key of {key}")
- 
-    note_index = get_note_offset(key)
+def build_key_dict(root: str) -> List[Tuple[str, str, str]]:
+    result: List[Tuple[str, str, str]] = []
+    note_index = get_note_offset(root)
     modal_index = 0 
 
     i = 0
 
-    while i < 7:
-        modal_index = (modal_index) % len(MODES)
-        mode = MODES[modal_index]
-        
-        next_interval = IONIAN_INTERVALS[modal_index]
-        note_index = (note_index + next_interval) % len(NOTES)
+    while i < len(IONIAN_STEPS):
         note = NOTES[note_index] 
         chord = CHORDS[modal_index]
+        mode = MODES[modal_index]
+
+        result.append((note, chord, mode))
         
-        print(note, " ", chord, " ", mode) 
-
-        i += 1
         modal_index += 1
+        modal_index = (modal_index) % len(MODES)
+        next_step = IONIAN_STEPS[modal_index]
+        note_index = (note_index + next_step) % len(NOTES)
+       
+        i += 1
 
+    return result
 
 def main():
     if len(sys.argv) != 2:
@@ -47,8 +51,11 @@ def main():
         sys.exit(1)
 
     key = sys.argv[1]
-
-    print_key(key)
+    print(f"Key: {key} Ionian")
+ 
+    key_data = build_key_dict(key)
+    print(tabulate(key_data, headers=["Note","Chord","Mode"], tablefmt="github"))
+    #pprint.pp(key_data)
 
 if __name__ == "__main__":
     main()
